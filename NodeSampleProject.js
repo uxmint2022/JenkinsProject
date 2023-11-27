6cb5ba16cf73eb172c446b3d41c7c660a12815ec
Abubakkar Sithiq <abubakkarsithiq66+git@gmail.com>
2023-11-27 10:48:10 +0530

    addding package.json

--- NodeSampleProject.js
+++ NodeSampleProject.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   // Send the response
-  res.end('Hello, World!  \n');
+  res.end('Hello, From Jenkins Sample Project! \n');
 
 });
 
