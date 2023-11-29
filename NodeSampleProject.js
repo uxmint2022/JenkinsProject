d9581890b884d6fad120ebc6258b484705b217f9
Abubakkar Sithiq <abubakkarsithiq66+git@gmail.com>
2023-11-29 12:50:31 +0530

    adding html file

--- NodeSampleProject.js
+++ NodeSampleProject.js
@@ -1,22 +1,24 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const router = express.Router()
 
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Define a route handling GET requests on the root path
 router.get('/', (req, res) => {
-  res.json({
-    message:'Hello, From Jenkins Sample Project! \n'
-  });
+  // res.json({ message:'Hello, From Jenkins Sample Project ! \n' });
+  res.sendFile(path.join(__dirname,'public','index.html'))
 });
+
 
 router.get('/webhook', (req, res) => {
-  res.send('Hello, From Jenkins Sample Project Webhook Route! \n');
+  res.send('Hello, From Jenkins Sample Project Webhook Page! \n');
 });
-
 
 
 // Start the server on port 3200
